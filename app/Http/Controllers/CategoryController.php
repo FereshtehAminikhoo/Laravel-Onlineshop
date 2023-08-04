36e7c5c155462bcb81d498865480aa1168c7fe46
Amin Hashemi <faminikhoo@gmail.com>
2023-08-04 17:25:51 +0330

    Add edit and delete functions in category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,16 +7,39 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function save(Request $request){
+    public function save(Request $request)
+    {
         Category::create([
-           'name'=>$request->title
+            'name' => $request->name
         ]);
-       return back();
+        return redirect()->route('category_list');
     }
 
     public function list()
     {
-        $categories=Category::all();
-        return view('admin.category.list',compact('categories'));
+        $categories = Category::all();
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function edit($id)
+    {
+        $category = Category::where('id', $id)->first();
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $category = Category::where('id', $id)->first();
+        $category->update([
+            'name' => $request->name
+        ]);
+        return redirect()->route('category_list');
+    }
+
+    public function delete($id)
+    {
+        $category = Category::where('id', $id)->first();
+        $category->delete();
+        return redirect()->route('category_list');
     }
 }
