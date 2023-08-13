5f39998aa8ae75999707434842dea8f5fb941edd
Amin Hashemi <faminikhoo@gmail.com>
2023-08-14 00:27:46 +0330

    Refactor user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'family_name',
+        'national_code',
+        'mobile',
         'password',
     ];
 
--- database/migrations/2014_10_12_0000080_create_users_table.php
+++ database/migrations/2014_10_12_0000080_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('family_name');
+            $table->string('national_code');
+            $table->string('mobile');
             $table->enum('type',['admin','user'])->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
